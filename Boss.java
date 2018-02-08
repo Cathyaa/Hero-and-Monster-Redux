47ae29308a9d792f4ef2bf9afc3753b30e0cd753
MatthewKim500 <matthewkim500@gmail.com>
2018-02-08 13:57:24 -0800

    Update Boss.java

--- Boss.java
+++ Boss.java
@@ -1,15 +1,9 @@
 public class Boss extends Monster
 {
-    Satchel satchelDrop = null;
-    Weapon weaponDrop = null;
-    Potion phoenixDrop = null;
-    public Boss(int atk, int hlth, int spd, Satchel drop1, Weapon drop2, Potion drop3)
+    public Boss(int atk, int hlth, int spd)
     {
         attack = atk;
         health = hlth;
         spd = speed;
-        satchelDrop = drop1;
-        weaponDrop = drop2;
-        phoenixDrop = drop3;
     }
 }
