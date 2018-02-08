9abf0dda3231a1d6085bab8d9e1e3ec92f03765f
Cathyaa <35276495+Cathyaa@users.noreply.github.com>
2018-02-08 13:37:21 -0800

    Update Monster.java

--- Monster.java
+++ Monster.java
@@ -1,6 +1,6 @@
 public class Monster{
     int attack, health, speed, str;
-    
+    String direction;
     public Monster(){
         attack = (int)(Math.random() * 30 + 1) - 10;
         if(attack <= 0){
@@ -9,6 +9,16 @@ public class Monster{
         health = (int)(Math.random() * 100 + 1);
         speed = (int)(Math.random() * 4);
         str = (int)(Math.random() * 4);
+        int prob = (int)(Math.random() * 4);
+        if(prob == 0){
+            direction = "w";
+        }else if(prob == 1){
+            direction = "a";
+        }else if(prob == 2){
+            direction = "s";
+        }else{
+            direction = "d";
+        }
     }
     
     public String toString(){
