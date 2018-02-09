7a1139f846256f8c039d1702b626fbd4ba345221
MatthewKim500 <matthewkim500@gmail.com>
2018-02-09 13:24:36 -0800

    Update Boss.java

--- Boss.java
+++ Boss.java
@@ -1,9 +1,11 @@
 public class Boss extends Monster
 {
-    public Boss(int atk, int hlth, int spd)
+    int gold;
+    public Boss(int atk, int hlth, int spd, int gld)
     {
         attack = atk;
         health = hlth;
         spd = speed;
+        gold = gld;
     }
 }
