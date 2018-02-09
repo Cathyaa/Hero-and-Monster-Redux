d75ee1fde02b552d5a9746f7c63bbe3c1e57d654
Cathyaa <35276495+Cathyaa@users.noreply.github.com>
2018-02-09 13:54:44 -0800

    Update Farmer.java

--- Farmer.java
+++ Farmer.java
@@ -1,16 +1,14 @@
 public class Farmer{
-    String type, lines;
-    public Farmer(int count){
-        if(count == 0){
-            lines = "My sheep have been taken… My family is gone and I have nothing left except" + 
-            "\n" + "for bronze amor I have buried… Only a true hero will receive this" + "\n" + 
-            "Please slain 2 monsters";
-            type = "armor";
-            Armor bronzeAmor = new Armor();
-        }else{
-            lines = "I will give you this long sword when you slain 4 monsters";
-            type = "sword";
-            LongSword lsword = new LongSword();
-        }
+    public Farmer(){
+        
+    }
+    public void interact(){
+        System.out.println("Hero meets a townspeople, and all position of townspeople is revealed");
+        System.out.println("You are welcomed to trade with me: ");
+        System.out.println("1.potion = 1 hides" + "\n" + 
+                            "2. bombs = 1 hides" + "\n" + 
+                            "3.breastplate = 3 hides" + "\n" +
+                            "4.satchel = 2 hidea" + "\n" +
+                            "5.leather sandals = 3 hides");
     }
 }
